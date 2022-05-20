a009da0a6ee6e83baa141444a685a769172c326b
bdoncer <barbara.doncer01@gmail.com>
2022-05-20 17:15:27 +0200

    Initial commit

---
+++ coefficients.py
@@ -0,0 +1,43 @@
+import numpy
+import pygad
+
+function_inputs = None
+desired_output = None
+
+def fitness_func(solution, solution_index):
+    if function_inputs == None or desired_output == None:
+        return 0
+    output = numpy.sum(solution * function_inputs)
+    return 1.0 / numpy.abs(output - desired_output)
+
+class Coefficients:
+    def __init__(self):
+        self.function_inputs = None
+        self.desired_output = None
+        self.sol_per_pop = 50
+        self.num_genes = None
+        self.init_range_low = -2
+        self.init_range_high = 5
+        self.mutation_percent_genes = 50
+        self.num_generations = 100
+        self.num_parents_mating = 10
+
+
+    def result_clicked(self):
+        global function_inputs, desired_output
+        function_inputs = self.function_inputs
+        desired_output = self.desired_output
+        ga_instance = pygad.GA(num_generations=self.num_generations,
+                               num_parents_mating=self.num_parents_mating,
+                               fitness_func=fitness_func,
+                               sol_per_pop=self.sol_per_pop,
+                               num_genes=self.num_genes,
+                               init_range_low=self.init_range_low,
+                               init_range_high=self.init_range_high,
+                               mutation_percent_genes=self.mutation_percent_genes)
+
+
+
+        ga_instance.run()
+        solution, _, _ = ga_instance.best_solution()
+        return solution
---
+++ gui.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from coefficients import Coefficients,function_inputs,desired_output
+
+
+class GUI:
+    def __init__(self):
+        self.coef = Coefficients()
+        self.picture_size = 420
+        self.root = tk.Tk()
+        self.root.geometry('1280x500')
+        self.root.title('Equation')
+
+
+        self.coefficients_text = tk.Text(self.root, height=2, width=20)
+        self.coefficients_label = tk.Label(self.root, text="Write all coefficients")
+        self.coefficients_label.config(font=("Courier", 14))
+        self.coefficients_label.pack()
+        self.coefficients_text.pack()
+
+        self.ex_res_text = tk.Text(self.root, height=2, width=10)
+        self.ex_res_label = tk.Label(self.root, text="Write expected result")
+        self.ex_res_label.config(font=("Courier", 14))
+        self.ex_res_label.pack()
+        self.ex_res_text.pack()
+
+        self.calc_button = tk.Button(self.root, text="Calculate result", command=self.calc_result)
+        self.calc_button.pack()
+
+        self.info_label = tk.Label(self.root, text="Result will be shown here:")
+        self.info_label.config(font=("Courier", 14))
+        self.info_label.pack()
+        self.res_label = tk.Label(self.root, text=" ")
+        self.res_label.config(font=("Courier", 14))
+        self.res_label.pack()
+        self.res2_label = tk.Label(self.root, text=" ")
+        self.res2_label.config(font=("Courier", 14))
+        self.res2_label.pack()
+
+        self.root.mainloop()
+
+
+    def calc_result(self):
+        genes = list(map(int, self.coefficients_text.get("1.0","end").split()))
+        self.coef.num_genes = len(genes)
+        self.coef.function_inputs = genes
+        self.coef.desired_output = int(self.ex_res_text.get("1.0","end"))
+
+        solution = self.coef.result_clicked()
+        self.res_label.config(text = solution)
+        self.info_label.config(text="")
+
+        res = 0
+        for i in range(len(genes)):
+            res += genes[i]*solution[i]
+        self.res2_label.config(text= res)
+
+
+
+gui = GUI()
