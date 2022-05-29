da105a808e63a9e2fddb4ab0b94604ca13b2f920
bdoncer <barbara.doncer01@gmail.com>
2022-05-29 14:21:57 +0200

    finding maximum added

---
+++ maxim.py
@@ -0,0 +1,43 @@
+from bees_algorithm import BeesAlgorithm
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def function(x_tab):
+    return [pow(xi,2) for xi in x_tab]
+
+def function_to_plot(x):
+    return  pow(x,2)
+
+
+
+
+
+class MaxCtr:
+    def __init__(self):
+        self.left_boundary = []
+        self.right_boundary = []
+        self.function = None
+
+
+    def result_clicked(self):
+        alg = BeesAlgorithm(function, self.left_boundary, self.right_boundary, ns=0, nb=14, ne=1, nrb=5, nre=30,
+                            stlim=10)
+
+        alg.performFullOptimisation(max_iteration=5000)
+
+        best = alg.best_solution
+        return best.values,best.score
+
+    def plot_function(self):
+        x_range = self.right_boundary[0] - self.left_boundary[0]
+        x_plot = [i for i in
+                  np.arange(self.left_boundary[0], self.right_boundary[0],
+                            x_range / 1000)]
+        x_plot.append(self.right_boundary[0])
+        y_plot = []
+        for x in x_plot:
+            y_plot.append(function_to_plot(x))
+
+        plt.plot(x_plot, y_plot, color="pink")
+        plt.show()
